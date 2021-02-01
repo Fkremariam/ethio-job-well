ee3cf5c8ce11f744d3b87f781ba4fe7489b7e53b
Fkremariam <wassihunfkremariam@gmail.com>
2021-02-01 19:58:47 +0300

    nav just done

---
+++ src/About/About.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function about() {
+    return(
+        <h2>This about page</h2>
+    );
+}
+export default about;
---
+++ src/ContactUs/contactUs.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function contactUs() {
+    return(
+        <h2>This is contactUs page</h2>
+    );
+}
+
+export default contactUs;
